e73eef16b8c50bd85ef2d98c0d2de74370498ed4
Chandra Sekhar A <chandrasekhar.akula@dnata.com>
2017-02-21 16:26:00 +0400

    done

--- www/js/app.js
+++ www/js/app.js
@@ -8,17 +8,18 @@ function init() {
 
 }
 
+
 function errorHandler(e) {
 	console.log("errorHandler: "+e);
 }
 
 function gotContacts(c) {
-	console.log("gotContacts, number of results "+c.length);
+	//console.log("gotContacts, number of results "+c.length);
 	picDiv = document.querySelector("#pictures");
 	for(var i=0, len=c.length; i<len; i++) {
 		console.dir(c[i]);
 		if(c[i].photos && c[i].photos.length > 0) {
-			picDiv.innerHTML += "<img src='"+c[i].photos[0].value+"'>";
+			picDiv.innerHTML += "<"+c[i].photos[0].value+"'>";
 		}
 	}
 }
